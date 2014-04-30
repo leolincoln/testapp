55675295a4940edabf6fc216b891e0863371d98b
liuliu <leoliu@u.northwestern.edu>
2014-04-30 01:15:12 -0500

    commit 3

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app=Flask(__name__)
 
@@ -7,9 +7,17 @@ app.config.update(
     DEBUG=True,
 )
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path,'static'),'ico/favicon.ico')
+
 @app.route('/')
-def hello():
-    return 'hello world2!'
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 
 if __name__ == '__main__':
