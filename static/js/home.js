b96736a21f7dbe157c8e23cb51676d4d9275ff50
liuliu <leoliu@u.northwestern.edu>
2014-04-30 13:23:31 -0500

    1pm

--- static/js/home.js
+++ static/js/home.js
@@ -3,7 +3,6 @@ var g1,g2,g3,g4,g5,g6;
 window.onload=function(){
     console.log("in g1");
     var g1=new JustGage({id:"g1",value:getRandomInt(0,100),min:0,max:100,title:"E bounce rate",label:"",levelColors:["#990000","#ffa500","#008000"]});
-
     var g2=new JustGage({id:"g2",value:getRandomInt(0,100),min:0,max:100,title:"E page/visit",label:"",levelColors:["#990000","#ffa500","#008000"]});
     var g3=new JustGage({id:"g3",value:getRandomInt(0,100),min:0,max:100,title:"E %new",label:"",levelColors:["#990000","#ffa500","#008000"]});
     var g4=new JustGage({id:"g4",value:getRandomInt(0,100),min:0,max:100,title:"%search",label:"",levelColors:["#990000","#ffa500","#008000"]});
