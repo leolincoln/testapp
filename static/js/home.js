b4914bfd8baf30940ecbbfc09ad248ba8ced0602
liuliu <leoliu@u.northwestern.edu>
2014-04-30 17:12:35 -0500

    updated letters

--- static/js/home.js
+++ static/js/home.js
@@ -2,12 +2,13 @@
 var g1,g2,g3,g4,g5,g6;
 window.onload=function(){
     console.log("in g1");
-    var g1=new JustGage({id:"g1",value:getRandomInt(0,100),min:0,max:100,title:"E bounce rate",label:"",levelColors:["#990000","#ffa500","#008000"]});
-    var g2=new JustGage({id:"g2",value:getRandomInt(0,100),min:0,max:100,title:"E page/visit",label:"",levelColors:["#990000","#ffa500","#008000"]});
-    var g3=new JustGage({id:"g3",value:getRandomInt(0,100),min:0,max:100,title:"E %new",label:"",levelColors:["#990000","#ffa500","#008000"]});
-    var g4=new JustGage({id:"g4",value:getRandomInt(0,100),min:0,max:100,title:"%search",label:"",levelColors:["#990000","#ffa500","#008000"]});
-    var g5=new JustGage({id:"g5",value:getRandomInt(0,100),min:0,max:100,title:"%social",label:"",levelColors:["#990000","#ffa500","#008000"]});
-    var g6=new JustGage({id:"g6",value:getRandomInt(0,100),min:0,max:100,title:"%referral",label:"",levelColors:["#990000","#ffa500","#008000"]});
+    var g1=new JustGage({id:"g1",value:getRandomInt(0,100),min:0,max:100,title:"Expected Bounce Rate",label:"",levelColors:["#990000","#ffa500","#008000"]});
+
+    var g2=new JustGage({id:"g2",value:getRandomInt(0,100),min:0,max:100,title:"Expected Pages/Visit",label:"",levelColors:["#990000","#ffa500","#008000"]});
+    var g3=new JustGage({id:"g3",value:getRandomInt(0,100),min:0,max:100,title:"Expected %New Visitors",label:"",levelColors:["#990000","#ffa500","#008000"]});
+    var g4=new JustGage({id:"g4",value:getRandomInt(0,100),min:0,max:100,title:"%View Search",label:"",levelColors:["#990000","#ffa500","#008000"]});
+    var g5=new JustGage({id:"g5",value:getRandomInt(0,100),min:0,max:100,title:"%View Social",label:"",levelColors:["#990000","#ffa500","#008000"]});
+    var g6=new JustGage({id:"g6",value:getRandomInt(0,100),min:0,max:100,title:"%View Referral",label:"",levelColors:["#990000","#ffa500","#008000"]});
 
     setInterval(function() {
             g1.refresh(getRandomInt(0, 100));
