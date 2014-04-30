75466bbf7171da5df8a01d714e964eb99405a183
liuliu <leoliu@u.northwestern.edu>
2014-04-30 00:56:34 -0500

    tutorial

--- hello.py
+++ hello.py
@@ -4,5 +4,13 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
+app.config.update(
+    DEBUG=True,
+    )
 def hello():
     return 'hello world2!'
+
+
+if __name == '__main__':
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port = port)
